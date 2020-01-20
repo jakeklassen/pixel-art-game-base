d69279e0d19b8aef8da1d81197b153cfaa222453
Jake Klassen <jklassendev@gmail.com>
2020-01-20 10:36:58 -0500

    fix fps at 50 with no smoothing

--- src/game.ts
+++ src/game.ts
@@ -72,47 +72,55 @@ loader.add(bunnyUrl).load((loader, resources) => {
     sprite: bunnyResource.data as HTMLImageElement,
   };
 
-  MainLoop.setUpdate((dt: number) => {
-    bunny.lastPos.x = bunny.pos.x;
-    bunny.lastPos.y = bunny.pos.y;
-    bunny.pos.x += (bunny.vel.x / dt) * bunny.dir.x;
-    bunny.pos.y += (bunny.vel.y / dt) * bunny.dir.y;
+  const TARGET_FPS = 50;
+  const STEP = 1000 / TARGET_FPS;
+  let last = performance.now();
+  let dt = 0;
 
-    if (bunny.pos.x + bunny.sprite.width >= GAME_WIDTH) {
-      bunny.pos.x = GAME_WIDTH - bunny.sprite.width;
-      bunny.dir.x *= -1;
-    } else if (bunny.pos.x <= 0) {
-      bunny.pos.x = 0;
-      bunny.dir.x *= -1;
+  function frame(hrt: DOMHighResTimeStamp) {
+    dt += hrt - last;
+
+    while (dt >= STEP) {
+      bunny.lastPos.x = bunny.pos.x;
+      bunny.lastPos.y = bunny.pos.y;
+      bunny.pos.x += (bunny.vel.x / STEP) * bunny.dir.x;
+      bunny.pos.y += (bunny.vel.y / STEP) * bunny.dir.y;
+
+      if (bunny.pos.x + bunny.sprite.width >= GAME_WIDTH) {
+        bunny.pos.x = GAME_WIDTH - bunny.sprite.width;
+        bunny.dir.x *= -1;
+      } else if (bunny.pos.x <= 0) {
+        bunny.pos.x = 0;
+        bunny.dir.x *= -1;
+      }
+
+      dt -= STEP;
     }
-  })
-    .setDraw((interpolationPercentage: number) => {
-      const x =
-        bunny.lastPos.x +
-        (bunny.pos.x - bunny.lastPos.x) * interpolationPercentage;
-      const y =
-        bunny.lastPos.y +
-        (bunny.pos.y - bunny.lastPos.y) * interpolationPercentage;
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = 'red';
-      ctx.lineWidth = 1;
-      ctx.beginPath();
-      ctx.moveTo(20 - 0.5, 0);
-      ctx.lineTo(20 - 0.5, GAME_HEIGHT);
-      ctx.moveTo(40 - 0.5, 0);
-      ctx.lineTo(40 - 0.5, GAME_HEIGHT);
-      ctx.moveTo(60 - 0.5, 0);
-      ctx.lineTo(60 - 0.5, GAME_HEIGHT);
-      ctx.moveTo(80 - 0.5, 0);
-      ctx.lineTo(80 - 0.5, GAME_HEIGHT);
-      ctx.stroke();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(20 - 0.5, 0);
+    ctx.lineTo(20 - 0.5, GAME_HEIGHT);
+    ctx.moveTo(40 - 0.5, 0);
+    ctx.lineTo(40 - 0.5, GAME_HEIGHT);
+    ctx.moveTo(60 - 0.5, 0);
+    ctx.lineTo(60 - 0.5, GAME_HEIGHT);
+    ctx.moveTo(80 - 0.5, 0);
+    ctx.lineTo(80 - 0.5, GAME_HEIGHT);
+    ctx.stroke();
 
-      ctx.translate(x | 0, y | 0);
+    ctx.translate(bunny.pos.x | 0, bunny.pos.y | 0);
 
-      ctx.drawImage(bunny.sprite, 0, 0);
+    ctx.drawImage(bunny.sprite, 0, 0);
 
-      ctx.setTransform(IDENTITY_MATRIX);
-    })
-    .start();
+    ctx.setTransform(IDENTITY_MATRIX);
+
+    last = hrt;
+
+    requestAnimationFrame(frame);
+  }
+
+  requestAnimationFrame(frame);
 });
