441e86b9f79305fccacebd49586a274e94547a57
Jake Klassen <jklassendev@gmail.com>
2021-06-09 09:13:33 -0400

    restore previous scaling logic

--- src/game.ts
+++ src/game.ts
@@ -5,13 +5,8 @@ import mapUrl from 'url:./assets/map.png';
 import megamanUrl from 'url:./assets/megaman.png';
 import { getResolution } from './lib/screen';
 
-const GAME_WIDTH = 384 * 2;
-const GAME_HEIGHT = 216 * 2;
-
-const SRC_GAME_WIDTH = 384;
-const SRC_GAME_HEIGHT = 216;
-
-let gameScale = 1;
+const GAME_WIDTH = 384;
+const GAME_HEIGHT = 216;
 
 const loader = new Loader();
 
@@ -25,10 +20,10 @@ canvas.style.width = `${GAME_WIDTH}px`;
 canvas.style.height = `${GAME_HEIGHT}px`;
 
 export const IDENTITY_MATRIX: DOMMatrix2DInit = {
-  a: 2,
+  a: 1,
   b: 0,
   c: 0,
-  d: 2,
+  d: 1,
   e: 0,
   f: 0,
 };
@@ -43,16 +38,14 @@ const resize = () => {
     GAME_HEIGHT,
   );
 
-  gameScale = factor;
-
   canvas.style.transform = `scale(${factor})`;
   canvas.style.left = `${innerWidth / 2 - canvas.width / 2}px`;
   canvas.style.top = `${innerHeight / 2 - canvas.height / 2}px`;
 };
 
-// resize();
+resize();
 
-// window.addEventListener('resize', resize);
+window.addEventListener('resize', resize);
 
 loader
   .add(bunnyUrl)
@@ -77,39 +70,42 @@ loader
         y: 1,
       },
       vel: {
-        x: 60 * 2,
-        y: 60 * 2,
+        x: 60,
+        y: 60,
       },
       sprite: playerResource.data as HTMLImageElement,
     };
 
     const TARGET_FPS = 60;
     const STEP = 1000 / TARGET_FPS;
+    const dt = STEP / 1000;
     let last = performance.now();
     let deltaTimeAccumulator = 0;
 
     function frame(hrt: DOMHighResTimeStamp) {
       deltaTimeAccumulator += Math.min(1000, hrt - last);
 
-      const dt = Math.min(1000, hrt - last) / 1000;
+      while (deltaTimeAccumulator >= STEP) {
+        player.pos.x += player.vel.x * dt * player.dir.x;
+        player.pos.y += player.vel.y * dt * player.dir.y;
 
-      player.pos.x += player.vel.x * dt * player.dir.x;
-      player.pos.y += player.vel.y * dt * player.dir.y;
+        if (player.pos.x + player.sprite.width >= GAME_WIDTH) {
+          player.pos.x = GAME_WIDTH - player.sprite.width;
+          player.dir.x *= -1;
+        } else if (player.pos.x <= 0) {
+          player.pos.x = 0;
+          player.dir.x *= -1;
+        }
 
-      if (player.pos.x + player.sprite.width * 2 >= GAME_WIDTH) {
-        player.pos.x = GAME_WIDTH - player.sprite.width * 2;
-        player.dir.x *= -1;
-      } else if (player.pos.x <= 0) {
-        player.pos.x = 0;
-        player.dir.x *= -1;
-      }
+        if (player.pos.y + player.sprite.height >= GAME_HEIGHT) {
+          player.pos.y = GAME_HEIGHT - player.sprite.height;
+          player.dir.y *= -1;
+        } else if (player.pos.y <= 0) {
+          player.pos.y = 0;
+          player.dir.y *= -1;
+        }
 
-      if (player.pos.y + player.sprite.height * 2 >= GAME_HEIGHT) {
-        player.pos.y = GAME_HEIGHT - player.sprite.height * 2;
-        player.dir.y *= -1;
-      } else if (player.pos.y <= 0) {
-        player.pos.y = 0;
-        player.dir.y *= -1;
+        deltaTimeAccumulator -= STEP;
       }
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -121,7 +117,7 @@ loader
       ctx.fillStyle = 'white';
       ctx.font = '10px Visitor';
 
-      ctx.setTransform(2, 0, 0, 2, player.pos.x, player.pos.y);
+      ctx.setTransform(1, 0, 0, 1, player.pos.x, player.pos.y);
 
       ctx.drawImage(player.sprite, 0, 0);
 
